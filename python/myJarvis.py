640fc1f917bcf2636da8a1118d8395ff790d0710
LucaDiStasio <luca.distasio@gmail.com>
2016-08-18 15:35:33 +0200

    synchronize

--- python/myJarvis.py
+++ python/myJarvis.py
@@ -1,6 +1,8 @@
 import os
 import pyttsx
+import speech_recognition as sr
 
+'''
 engine = pyttsx.init()
 engine.setProperty('rate', 70)
 
@@ -9,6 +11,12 @@ user = os.getenv('username')
 #1 -> Italian
 #2 -> English
 voices = engine.getProperty('voices')
+voice = voices[1]
+engine.setProperty('voice', voice.id)
+engine.say("Hi " + user + "! How are you today?")
+
+'''
+'''
 for voice in voices:
     print "Using voice:", repr(voice)
     engine.setProperty('voice', voice.id)
@@ -19,4 +27,71 @@ for voice in voices:
     engine.say("Sunday Monday Tuesday Wednesday Thursday Friday Saturday")
     engine.say("Violet Indigo Blue Green Yellow Orange Red")
     engine.say("Apple Banana Cherry Date Guava")
+'''
+'''
 engine.runAndWait()
+
+'''
+
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+# recognize speech using Sphinx
+try:
+    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
+
+# recognize speech using Google Speech Recognition
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+# recognize speech using Wit.ai
+WIT_AI_KEY = "INSERT WIT.AI API KEY HERE" # Wit.ai keys are 32-character uppercase alphanumeric strings
+try:
+    print("Wit.ai thinks you said " + r.recognize_wit(audio, key=WIT_AI_KEY))
+except sr.UnknownValueError:
+    print("Wit.ai could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Wit.ai service; {0}".format(e))
+
+# recognize speech using Microsoft Bing Voice Recognition
+BING_KEY = "INSERT BING API KEY HERE" # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
+try:
+    print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
+except sr.UnknownValueError:
+    print("Microsoft Bing Voice Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+
+# recognize speech using api.ai
+API_AI_CLIENT_ACCESS_TOKEN = "INSERT API.AI API KEY HERE" # api.ai keys are 32-character lowercase hexadecimal strings
+try:
+    print("api.ai thinks you said " + r.recognize_api(audio, client_access_token=API_AI_CLIENT_ACCESS_TOKEN))
+except sr.UnknownValueError:
+    print("api.ai could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from api.ai service; {0}".format(e))
+
+# recognize speech using IBM Speech to Text
+IBM_USERNAME = "INSERT IBM SPEECH TO TEXT USERNAME HERE" # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+IBM_PASSWORD = "INSERT IBM SPEECH TO TEXT PASSWORD HERE" # IBM Speech to Text passwords are mixed-case alphanumeric strings
+try:
+    print("IBM Speech to Text thinks you said " + r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
+except sr.UnknownValueError:
+    print("IBM Speech to Text could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from IBM Speech to Text service; {0}".format(e))
