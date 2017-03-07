2c3322ff9fe717e6e9a51b1313467438cf56d5cd
Luca Di Stasio <distasio1@univ-lorraine.fr>
2017-03-07 16:39:02 +0100

    committing changes on 7/3/2017 at 16:39:2

--- python/add_to_octave_startup.py
+++ python/add_to_octave_startup.py
@@ -60,7 +60,7 @@ def add_to_octave_startup(userName,root,WD):
             dirs.append(dirName)
     octavestart = join(root,octavepathDir,octavestartup)
     try:
-        with open(matlabstart, 'w') as file:
+        with open(octavestart, 'w') as file:
             file.write("disp('Hi, " + userName + "!');\n")
             file.write("disp('Welcome back!');\n")
             file.write("disp('I''m adding the following directories to the path:');\n")
@@ -70,7 +70,7 @@ def add_to_octave_startup(userName,root,WD):
                 file.write("disp('      " + dir + "');\n") 
                 file.write("addpath('" + dir + "','-end');\n")
     except Exception,e:
-        logErrorMessage(WD,logfile,'add_to_matlab_startup','add_to_octave_startup',e)
+        logErrorMessage(WD,logfile,'add_to_octave_startup','add_to_octave_startup',e)
         sys.exc_clear()
     
   
