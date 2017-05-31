2c4cd0db5cccbbfbe1dc3220f97e6f9cc725af00
Luca Di Stasio <distasio1@univ-lorraine.fr>
2017-05-31 21:00:46 +0200

    committing changes on 31/5/2017 at 21:0:46

--- python/scanReceipts.py
+++ python/scanReceipts.py
@@ -33,7 +33,8 @@ Tested with Python 2.7 Anaconda 2.4.1 (64-bit) distribution
 
 from os import listdir
 from os.path import isfile, join
-import numpy
+import argparse
+import numpy as np
 import cv2
 try:
     import Image
@@ -41,10 +42,71 @@ except ImportError:
     from PIL import Image
 import pytesseract
 
+def order_points(pts):
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype = "float32")
+ 
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis = 1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+ 
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    vdiff = np.diff(pts, axis = 1)
+    rect[1] = pts[np.argmin(vdiff)]
+    rect[3] = pts[np.argmax(vdiff)]
+ 
+    # return the ordered coordinates
+    return rect
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+ 
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+ 
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+ 
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype = "float32")
+ 
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+ 
+    # return the warped image
+    return warped
+
 cv2.destroyAllWindows()
 
-#wd = 'C:\\01_Backup-folder\\GoogleDrive\\receipts'
-wd = 'D:\\GoogleDrive\\receipts'
+wd = 'C:\\01_Backup-folder\\GoogleDrive\\receipts'
+#wd = 'D:\\GoogleDrive\\receipts'
 
 fileFormat = 'jpg'
 
@@ -53,11 +115,21 @@ for file in listdir(wd):
     if file.split('.')[1]==fileFormat:
         files.append(file)
 
+image = cv2.imread(join(wd,file),0)    
+pts = np.array([(73, 239), (356, 117), (475, 265), (187, 443)], dtype = "float32")
+
+warped = four_point_transform(image, pts)
+
+cv2.imshow("Receipt", image)
+cv2.imshow("Warped", warped)
+cv2.waitKey(0)
+'''
 for file in files[:1]:
     #image = cv2.imread(join(wd,file),0)
     #cv2.imshow("Receipt", image)
     #cv2.waitKey(0)
     print(join(wd,file))
     print(pytesseract.image_to_string(Image.open(join(wd,file)),lang='fra',config="-psm 6"))
+'''
 
 cv2.destroyAllWindows()
