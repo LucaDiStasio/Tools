af25d06b2126d300c006ab4a0b84216fa3d161f4
LucaDiStasio <luca.distasio@gmail.com>
2017-08-08 20:47:39 +0200

    committing changes on 8/8/2017 at 20:47:39

--- bash/compressPDFs.sh
+++ bash/compressPDFs.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+: '
+=====================================================================================
+
+Copyright (c) 2016 - 2017 Luca Di Stasio
+Author: Luca Di Stasio <luca.distasio@gmail.com>
+                       <luca.distasio@ingpec.eu>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=====================================================================================
+
+DESCRIPTION
+
+Compress a series of pdf files.
+
+Tested in Ubuntu 14.04
+'
+
 clear
 
 PDFSETTINGS="/ebook"
@@ -22,7 +51,7 @@ declare -a FILESWITHOUTEXT=("Pole-emploi_Ouverture-droit-allocation_2017-03-29"
                             "Lavot_psychiatre_2016-10-17"
                             "Lavot_antidepressive_2016-10-15(1)"
                             )
-                 
+
 for f in ${FILESWITHOUTEXT[*]}; do
     INP="$f"
     OUT="$f"
---
+++ bash/convertMatToPy.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+: '
+=====================================================================================
+
+Copyright (c) 2016 - 2017 Luca Di Stasio
+Author: Luca Di Stasio <luca.distasio@gmail.com>
+                       <luca.distasio@ingpec.eu>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=====================================================================================
+
+DESCRIPTION
+
+Convert Matlab/Octave source code to Python Numpy/Scipy.
+
+Tested in Ubuntu 14.04
+'
+
+function joinBy {
+  local IFS="$1"; shift; echo "$*";
+}
+
+function readContentDir {
+  for content in $1/*
+  do
+    if [ -d $content ]
+    then
+      echo "In directory "$content
+      readContentDir $content
+    elif [[ -f $content && ${content: -2} == ".m" && "$content" != *"ABQ"* ]]
+    then
+      IFS='/' read -a path <<< "$content"
+      echo "Matlab .m file "${path[-1]}" in repo "${path[4]}", folder "${path[5]}
+      replaceWith="python"
+      pyExt=".py"
+      localPython=${content//.m/$pyExt}
+      pythonContent=${content//matlab/$replaceWith}
+      IFS='/' read -a pythonFilePath <<< "$pythonContent"
+      IFS='/' read -a localPythonFilePath <<< "$localPython"
+      pythonPath="${pythonFilePath[0]}"
+      for i in "${!pythonFilePath[@]}"; do
+        if [[ $i >  0 && $i -lt $(( ${#pythonFilePath[@]}-1 )) ]]; then
+          pythonPath=$pythonPath"/"${pythonFilePath[$i]}
+        fi
+      done
+      echo "File will be translated in local file "$localPython
+      echo "Python folder to be created if it does not exist: "$pythonPath
+      echo "File will then be copied to "$pythonPath"/"${localPythonFilePath[-1]}
+    fi
+  done
+}
+
+WD="/home/luca/WD"
+
+readContentDir $WD
