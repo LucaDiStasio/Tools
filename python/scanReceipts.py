ad312000906d34dc1f9427ea55a474561c6e0e8f
LucaDiStasio <luca.distasio@gmail.com>
2017-08-08 10:29:07 +0200

    committing changes on 8/8/2017 at 10:29:7

--- python/scanReceipts.py
+++ python/scanReceipts.py
@@ -3,7 +3,7 @@
 '''
 =====================================================================================
 
-Copyright (c) 2016 Luca Di Stasio
+Copyright (c) 2016 - 2017 Luca Di Stasio
 Author: Luca Di Stasio <luca.distasio@gmail.com>
                        <luca.distasio@ingpec.eu>
 
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 DESCRIPTION
 
-Automatic translation of Latex documents.
+Receipts scanning
 
 Tested with Python 2.7 Anaconda 2.4.1 (64-bit) distribution
        in Windows 10.
@@ -56,20 +56,20 @@ def order_points(pts):
     # the second entry is the top-right, the third is the
     # bottom-right, and the fourth is the bottom-left
     rect = np.zeros((4, 2), dtype = "float32")
- 
+
     # the top-left point will have the smallest sum, whereas
     # the bottom-right point will have the largest sum
     s = pts.sum(axis = 1)
     rect[0] = pts[np.argmin(s)]
     rect[2] = pts[np.argmax(s)]
- 
+
     # now, compute the difference between the points, the
     # top-right point will have the smallest difference,
     # whereas the bottom-left will have the largest difference
     vdiff = np.diff(pts, axis = 1)
     rect[1] = pts[np.argmin(vdiff)]
     rect[3] = pts[np.argmax(vdiff)]
- 
+
     # return the ordered coordinates
     return rect
 
@@ -78,21 +78,21 @@ def four_point_transform(image, pts):
     # individually
     rect = order_points(pts)
     (tl, tr, br, bl) = rect
- 
+
     # compute the width of the new image, which will be the
     # maximum distance between bottom-right and bottom-left
     # x-coordiates or the top-right and top-left x-coordinates
     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
     widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
     maxWidth = max(int(widthA), int(widthB))
- 
+
     # compute the height of the new image, which will be the
     # maximum distance between the top-right and bottom-right
     # y-coordinates or the top-left and bottom-left y-coordinates
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
     maxHeight = max(int(heightA), int(heightB))
- 
+
     # now that we have the dimensions of the new image, construct
     # the set of destination points to obtain a "birds eye view",
     # (i.e. top-down view) of the image, again specifying points
@@ -103,11 +103,11 @@ def four_point_transform(image, pts):
         [maxWidth - 1, 0],
         [maxWidth - 1, maxHeight - 1],
         [0, maxHeight - 1]], dtype = "float32")
- 
+
     # compute the perspective transform matrix and then apply it
     M = cv2.getPerspectiveTransform(rect, dst)
     warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
- 
+
     # return the warped image
     return warped
 
@@ -124,7 +124,7 @@ for file in listdir(wd):
         files.append(file)
 
 
-image = cv2.imread(join(wd,file),0)    
+image = cv2.imread(join(wd,file),0)
 pts = np.array([(73, 239), (356, 117), (475, 265), (187, 443)], dtype = "float32")
 
 warped = four_point_transform(image, pts)
@@ -161,11 +161,11 @@ def processRequest( json, data, headers, params ):
 
         if response.status_code == 429:
             print( "Message: %s" % ( response.json() ) )
-            if retries <= _maxNumRetries: 
-                time.sleep(1) 
+            if retries <= _maxNumRetries:
+                time.sleep(1)
                 retries += 1
                 continue
-            else: 
+            else:
                 print( 'Error: failed after retrying!' )
                 break
         elif response.status_code == 202:
@@ -174,7 +174,7 @@ def processRequest( json, data, headers, params ):
             print( "Error code: %d" % ( response.status_code ) )
             print( "Message: %s" % ( response.json() ) )
         break
-        
+
     return result
 
 def getOCRTextResult( operationLocation, headers ):
@@ -208,7 +208,7 @@ def getOCRTextResult( operationLocation, headers ):
         break
 
     return result
-    
+
 #wd = 'C:\\01_Backup-folder\\GoogleDrive\\receipts'
 wd = 'D:\\GoogleDrive\\receipts'
 
---
+++ python/synchronizeReceipts.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+'''
+=====================================================================================
+
+Copyright (c) 2016 - 2017 Luca Di Stasio
+Author: Luca Di Stasio <luca.distasio@gmail.com>
+                       <luca.distasio@ingpec.eu>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=====================================================================================
+
+DESCRIPTION
+
+Synchronize receipts with web accounting apps.
+
+Tested with Python 2.7 Anaconda 2.4.1 (64-bit) distribution
+       in Windows 10.
+
+'''
